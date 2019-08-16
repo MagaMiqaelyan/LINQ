0b319230c8e61aa1a28e17edf3a56604a21477b5
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-08-16 17:27:48 +0400

    query to array

---
+++ LINQ/QueryToArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QueryToArray
+{
+    internal class Program
+    {
+        public static IEnumerable<string> GetCity(string[] cities)
+        {
+            return from city in cities
+                   where city.Contains('n') && city.EndsWith('e')
+                   select city;
+        }
+
+        private static void Main(string[] args)
+        {
+            string[] cities = { "Yerevan", "Rome", "Milan", "Florence", "Venice", "Athens" };
+
+            /// SQL syntax
+            foreach (var city in GetCity(cities))
+            {
+                Console.Write(city + " ");
+            }
+            Console.WriteLine();
+
+            /// Method syntax
+            var query = cities.Where(city => city.Contains('n') && city.EndsWith('e'));
+            foreach (var city in query)
+            {
+                Console.Write(city + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
